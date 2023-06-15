30d61aaad1a932874b430f1740f719f7356a2518
Dingwen Wang <wangdingwen32@gmail.com>
2023-06-15 14:10:06 +0800

    Add Redis cache

--- src/main/java/com/example/demo/service/UserDataService.java
+++ src/main/java/com/example/demo/service/UserDataService.java
@@ -13,6 +13,8 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import com.google.gson.Gson;
+
 
 @Service
 @Slf4j
@@ -26,7 +28,7 @@ public class UserDataService {
     private UserDataRepository userDataRepository;
 
     public UserData setUser(String username, int age) {
-        UserData newUser = this.findUserByUsername(username);
+        UserData newUser = this._findUserByUsername(username);
         // check if there is already a user
         if (newUser == null) {
             newUser = new UserData();
@@ -34,21 +36,40 @@ public class UserDataService {
         }
         newUser.setAge(age);
         mongoTemplate.save(newUser);
+        // delete redis cache
+        redisTemplate.delete(username);
         return newUser;
     }
 
     public UserData findUserByUsername(String username) {
-        return mongoTemplate.findById(username, UserData.class);
+        // check if there is already a user in Redis cache
+        String cachedUser = redisTemplate.opsForValue().get(username);
+        if (cachedUser != null) {
+            return new Gson().fromJson(cachedUser, UserData.class);
+        }
+        // if not, query from MongoDB
+        UserData user = this._findUserByUsername(username);
+        // save to Redis cache
+        if (user != null) {
+            redisTemplate.opsForValue().set(username, new Gson().toJson(user));
+        }
+        return user;
     }
 
     public boolean removeUser(String username) {
-        UserData res = this.findUserByUsername(username);
+        UserData res = this._findUserByUsername(username);
         if (res != null) {
             mongoTemplate.remove(res);
         } else {
             return false;
         }
+        // delete from redis cache
+        redisTemplate.delete(username);
         return true;
+    }
+
+    private UserData _findUserByUsername(String username) {
+        return mongoTemplate.findById(username, UserData.class);
     }
 
 }
